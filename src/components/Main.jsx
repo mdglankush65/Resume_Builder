8fdd93cd80affe7bf4a5f6d6bf0b39ab4b92f1fd
Ankush Mudgil <101817825+mdglankush65@users.noreply.github.com>
2023-10-08 12:27:03 +0530

    Folder Structure Added

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+      This is the main component of the homePage which contains templates.
+    </div>
+  )
+}
+
+export default Main
---
+++ src/components/subRoutes/Education.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Education = () => {
+  return (
+    <div>
+      It contains the Education part of the resume.
+    </div>
+  )
+}
+
+export default Education
