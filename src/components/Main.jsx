199824a137aa3c77c757e98bbb1081148849e942
Amanchahar1507 <amanchahar1702@gmail.com>
2023-10-11 21:10:42 +0530

    Experience Part of Resume Designed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,41 @@
 import React from 'react'
+<<<<<<< HEAD
 import Experience from "./subRoutes/Experience";
 
 import { Container } from '@mui/material';
+=======
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Templates from './Templates';
+>>>>>>> 5f56519483c03c3d2bde07c2125e0fc5875064c1
 
+const array=[1,2,3,4,5,6,7,8,9,10,12,13,14,15];
 const Main = () => {
  
   return (
+<<<<<<< HEAD
     <Container>
     <Experience/>
   </Container>
+=======
+    <Container disableGutters maxWidth="sm" component="main" sx={{ pt: 8, pb: 6 }}>
+      <Typography
+        component="h1"
+        variant="h2"
+        align="center"
+        color="text.primary"
+        gutterBottom
+      >
+        Resume Templates
+      </Typography>
+      <Typography variant="h5" align="center" color="text.secondary" component="p">
+        Explore our meticulously designed resume templates to effortlessly create a professional and eye-catching CV that stands out to potential employers.
+      </Typography>
+      <Container sx={{display: `flex`}}>
+        {array.map((val) => <Templates count={`${val}`} key={val} />)};
+      </Container>
+    </Container>
+>>>>>>> 5f56519483c03c3d2bde07c2125e0fc5875064c1
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import GlobalStyles from '@mui/material/GlobalStyles';
 
-const Navbar = () => {
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function Navbar() {
   return (
-    <div>
-      This is the navbar component.
-    </div>
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+      <CssBaseline />
+      <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        sx={{ borderBottom: (theme) => `0px solid ${theme.palette.divider}` }}
+      >
+        <Toolbar sx={{ flexWrap: 'wrap' }}>
+          <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+            Resume Builder
+          </Typography>
+          <nav>
+            <Link
+              variant="button"
+              color="text.primary"
+              href="#"
+              sx={{ my: 1, mx: 1.5, textDecoration: `none` }}
+            >
+              Resume Templates
+            </Link>
+            <Link
+              variant="button"
+              color="text.primary"
+              href="/MyResume"
+              sx={{ my: 1, mx: 1.5, textDecoration: `none` }}
+            >
+              My Resumes
+            </Link>
+            <Link
+              variant="button"
+              color="text.primary"
+              href="/About"
+              sx={{ my: 1, mx: 1.5, textDecoration: `none` }}
+            >
+              About Us
+            </Link>
+          </nav>
+        </Toolbar>
+      </AppBar>
+    </ThemeProvider>
+  );
 }
-
-export default Navbar
