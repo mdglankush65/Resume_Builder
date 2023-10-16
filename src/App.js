22fd9b985dc25a908097a4fd5d7ef221f3143a42
Amanchahar1507 <amanchahar1702@gmail.com>
2023-10-16 23:33:54 +0530

    create the keyskill subRoutes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Main from './components/Main';
 import Footer from './components/Footer';
 import Info from './components/subRoutes/Info';
 import Education from './components/subRoutes/Education';
-import Keyskills from './components/subRoutes/Keyskill';
 import Work_exe from './components/subRoutes/Work_exe';
 
 function App() {
--- src/components/subRoutes/Keyskill.js
+++ src/components/subRoutes/Keyskill.js
@@ -1,59 +1,104 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
-import './../../css/keyskill.css';
-
-library.add(faTrash);
-
-function Keyskill() {
-    const [skills, setSkills] = useState(['', '']);
-
-    const addSkill = () => {
-        setSkills([...skills, '']);
-    };
-
-    const removeSkill = (index) => {
-        const updatedSkills = [...skills];
-        updatedSkills.splice(index, 1);
-        setSkills(updatedSkills);
-    };
-
-    const handleSkillChange = (index, value) => {
-        const updatedSkills = [...skills];
-        updatedSkills[index] = value;
-        setSkills(updatedSkills);
-    };
-
-    return (
-        <div className='key-container'>
-            <h1>Key Skill</h1>
-            <hr />
-
-            {skills.map((skill, index) => (
-                <div key={index} className='skill-row'>
-                    <input
-                        type="text"
-                        value={skill}
-                        onChange={(e) => handleSkillChange(index, e.target.value)}
-                    />
-
-                    {index !== 0 && (
-                        <button className="remove-button" onClick={() => removeSkill(index)}>
-                            <FontAwesomeIcon icon="trash" />
-                        </button>
-                    )}
-                </div>
-            ))}
-            <div className=' allbutton'>
-                <button onClick={addSkill} className='addnewbtn'>Add New</button>
-                <div className="button-container">
-                    <button className="back">Back</button>
-                    <button className="next">Next</button>
-                </div>
+import React, { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+import {
+  Button,
+  TextField,
+  Box,
+  Container,
+  Typography,
+  IconButton,
+  Divider,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+const Keyskill = () => {
+  const { control, handleSubmit, reset } = useForm();
+  const [sections, setSections] = useState([{ id: 1, field: "Experience1" }]);
+  const addNewSection = () => {
+    const changeid = sections.length + 1;
+    setSections([...sections, { id: changeid, field: "" }]);
+  };
+  const removeSection = (id) => {
+    const changeSection = sections.filter((section) => section.id !== id);
+    setSections(changeSection);
+  };
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
+  return (
+    <Box
+      component="form"
+      sx={{
+        width: 500,
+        ml: 60,
+        mt: 15,
+        boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.6)",
+      }}
+    >
+      <Container>
+        <Typography variant="4" gutterBottom="true">
+          <span
+            style={{ fontSize: "25px", fontWeight: "bold", marginTop: "60px" }}
+          >
+            Key Skills
+          </span>
+        </Typography>
+        <Divider style={{ marginBottom: "20px" }} />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {sections.map((section,index) => (
+            <div key={section.id}>
+              <Controller
+                name={`skills[${index}].skill`}
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                <TextField
+                sx={{ mt: 5, ml: 10, width: 250,
+                    "& .MuiInputBase-root": {
+                        height: 15
+                    }}}
+                {...field}
+                id="outlined-multiline-flexible"
+                label="Skills"
+                multiline
+                />
+                )}
+                /> 
+              <IconButton
+                onClick={() => removeSection(section.id)}
+                sx={{ mt: 4 }}
+              >
+                <DeleteIcon />
+              </IconButton>
             </div>
-        </div>
-    );
-}
+          ))}
+        </form>
 
+      </Container>
+      <Button
+        variant="contained"
+        onClick={addNewSection}
+        sx={{ ml: 25, mt: 2,px:2,py:0.5 }}
+        style={{fontSize:'10px'}}
+      >
+        ADD NEW
+      </Button>
+      <Box sx={{ ml: 33, mt: 1, mb: 2, p: 2 }}
+      style={{fontSize:'10px'}}>
+        <Button variant="outlined" sx={{ px: 2,py:0.5 }}
+        style={{fontSize:'10px'}}>
+          Back
+        </Button>
+        <Button variant="contained" sx={{ ml: 2 ,px:2,py:0.5}}
+        style={{fontSize:'10px'}}
+        >
+          Submit
+        </Button>
+      </Box>
+    </Box>
+  );
+};
 export default Keyskill;
+
+
+
