8b83f3f37836ba2264c3e9d301a86e6cca9abd60
Amanchahar1507 <amanchahar1702@gmail.com>
2023-10-17 13:50:03 +0530

    keyskill subRoutes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from './components/Footer';
 import Info from './components/subRoutes/Info';
 import Education from './components/subRoutes/Education';
 import Work_exe from './components/subRoutes/Work_exe';
-
+import InvoiceForm from './components/subRoutes/Info';
 function App() {
     return (
         <div className="App">
@@ -14,6 +14,7 @@ function App() {
             {/* <Education /> */}
             {/* <Keyskills /> */}
             {/* <Work_exe /> */}
+            <InvoiceForm/>
             <Main />
             <Footer />
         </div>
--- src/components/subRoutes/Info.js
+++ src/components/subRoutes/Info.js
@@ -162,4 +162,3 @@ function Info() {
 
 export default Info;
 
-
